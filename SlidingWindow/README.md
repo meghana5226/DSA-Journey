a623679f74171046079c62a15156b5a6b8d46934
meghana5226 <238r1a12d7meghana@gmail.com>
2025-06-29 16:05:51 +0530

    Added Sliding Window notes and problems

---
+++ SlidingWindow/README.md
@@ -0,0 +1,12 @@
+## Sliding Window Pattern
+
+# Use-case:
+- Used to find subarrays with max/min sum, length, or conditions.
+
+###  Types:
+- Fixed Size Sliding Window
+- Variable Size Sliding Window
+
+###  Problems Solved:
+1. Maximum Sum Subarray of Size K
+2. Longest Substring Without Repeating Characters
---
+++ SlidingWindow/SlidingWindowProblems.java
@@ -0,0 +1,11 @@
+// Max sum of subarray of size K
+public int maxSum(int[] arr, int k) {
+    int sum = 0, maxSum = 0;
+    for (int i = 0; i < k; i++) sum += arr[i];
+    maxSum = sum;
+    for (int i = k; i < arr.length; i++) {
+        sum += arr[i] - arr[i - k];
+        maxSum = Math.max(maxSum, sum);
+    }
+    return maxSum;
+}
