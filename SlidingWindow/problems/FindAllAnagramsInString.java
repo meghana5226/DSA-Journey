17df642b52e8f02e6307340a558285ef6f4dd433
meghana5226 <238r1a12d7meghana@gmail.com>
2025-06-30 15:47:05 +0530

    Added Fixed Size Sliding Window problems: Max Sum & Anagrams

---
+++ SlidingWindow/problems/FindAllAnagramsInString.java
@@ -0,0 +1,49 @@
+package SlidingWindow.problems;
+import java.util.*;
+public class FindAllAnagramsInString {
+    
+    // Problem: Find All Anagrams in a String
+// Link: https://leetcode.com/problems/find-all-anagrams-in-a-string/
+// Pattern: Fixed Size Sliding Window + Frequency Count
+
+
+    public static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+
+        if (p.length() > s.length()) return result;
+
+        int[] pCount = new int[26];
+        int[] sCount = new int[26];
+
+        // Build initial window
+        for (int i = 0; i < p.length(); i++) {
+            pCount[p.charAt(i) - 'a']++;
+            sCount[s.charAt(i) - 'a']++;
+        }
+
+        // Check first window
+        if (Arrays.equals(pCount, sCount)) result.add(0);
+
+        // Slide window
+        for (int i = p.length(); i < s.length(); i++) {
+            sCount[s.charAt(i) - 'a']++; // add right
+            sCount[s.charAt(i - p.length()) - 'a']--; // remove left
+
+            if (Arrays.equals(pCount, sCount)) {
+                result.add(i - p.length() + 1);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "cbaebabacd";
+        String p = "abc";
+
+        List<Integer> indices = findAnagrams(s, p);
+        System.out.println("Anagram indices: " + indices);
+    }
+}
+
+
---
+++ SlidingWindow/problems/MaxSumSubarrayOfSizeK.java
@@ -0,0 +1,36 @@
+package SlidingWindow.problems;
+
+
+    // Problem: Maximum Sum Subarray of Size K
+// Link: https://leetcode.com/problems/maximum-average-subarray-i/
+// Pattern: Fixed Size Sliding Window
+
+public class MaxSumSubarrayOfSizeK {
+
+    public static int maxSum(int[] arr, int k) {
+        int sum = 0, max = 0;
+
+        // Step 1: Get sum of first window
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        max = sum;
+
+        // Step 2: Slide the window
+        for (int i = k; i < arr.length; i++) {
+            sum += arr[i] - arr[i - k]; // slide
+            max = Math.max(max, sum);
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 5, 1, 3, 2};
+        int k = 3;
+        System.out.println("Max Sum of size " + k + " = " + maxSum(arr, k));
+    }
+}
+
+
